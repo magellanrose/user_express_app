5aa3c4cb7efd962f3f68d414ea2641e2fec23f54
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-01-23 10:02:20 -0500

    added schema file

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS mysql_first_day_db;
+
+CREATE DATABASE mysql_first_day_db;
+
+USE mysql_first_day_db;
+
+CREATE TABLE users (
+    username VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL
+);
+
+INSERT INTO users (username, email, password) VALUES
+('NV', 'nv@test.com', 'password123'),
+('ST', 'st@test.com', 'password123'),
+('LV', 'lv@test.com', 'password123');
+
+SELECT * FROM users;
