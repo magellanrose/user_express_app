0a8e76a976514dd0c76941c123ae05b80bc91bf3
Nicholas Vacca <nicholasfvacca@gmail.com>
2024-01-25 10:23:16 -0500

    pushing up for render

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const is_prod = process.env.NODE_ENV === 'production;'
 
 //Create connection to our mysql database
 const connection = mysql.createConnection({
     host: process.env.DB_HOST_URL,
     user: process.env.DB_USER,
-    password: is_prod ? process.env.DB_PASSWORD : '',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DATABASE_NAME
 });
 
